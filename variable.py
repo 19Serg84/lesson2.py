423fc02a105a134b0b9579fd0fb4b87c3d204a09
19Serg84 <pagalin.sergey@yandex.ru>
2024-08-03 17:18:24 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,4 @@
+numder_of_tesk = 12
+hours = 1.5
+course_name = 'Python'
+print('КУРС:',course_name,'.' , 'Всего задач:', numder_of_tesk, '. ' 'затрачено часов:', hours,'. ' 'среднее время выполнения:', hours/numder_of_tesk)
